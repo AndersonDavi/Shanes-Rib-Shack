8e7fdb081f77a9ad4e97d9e658ede0df9a70dcb3
AndersonDavi <andesondavid17@gmail.com>
2021-10-25 18:05:28 -0500

    Validación inputs de registro

--- App.py
+++ App.py
@@ -8,6 +8,7 @@ from flask import (
         g
 )
 import os
+import re
 from flask.helpers import flash
 from flask.wrappers import Request
 # from flask_login import LoginManager
@@ -21,6 +22,29 @@ app.config['MYSQL_DB']= 'shanesribdb'
 mysql = MySQL(app)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
+
+# Comprobar inputs
+def es_correo_valido(correo):
+    expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    return re.match(expresion_regular, correo) is not None
+
+def RegisterValidate(name,lastname,email,password,password2):
+        RegisterBool = ""
+        if len(name)<1:
+                RegisterBool = "Rellene todos los campos"
+        elif len(lastname)<1:
+                RegisterBool = "Rellene todos los campos"
+        elif len(email)<1:
+                RegisterBool = "Rellene todos los campos"
+        elif es_correo_valido(email) == False:
+                RegisterBool = "Digite un correo válido"
+        elif len(password)<1:
+                RegisterBool = "Rellene todos los campos"
+        elif len(password)<6:
+                RegisterBool = "La contraseña debe ser mínimo de 6 caracteres"
+        elif password2 != password:
+                RegisterBool = "Las contraseñas no coinciden"
+        return RegisterBool
 
 # @app.route('/')
 # def index():
@@ -104,13 +128,30 @@ def addUser():
                 lastname = request.form['lastname']
                 email = request.form['email']
                 password = request.form['password']
-                cur = mysql.connection.cursor()
-                cur.execute('INSERT INTO users (name, lastname, email, password) VALUES (%s,%s,%s,%s)', (name,lastname,email,password))
-                mysql.connection.commit()
-                flash('Registrado correctamente')
-                # session['user'] = name
+                password2 = request.form['password2']
+                ErrorMessage = RegisterValidate(name,lastname,email,password,password2)
 
-                return redirect(url_for('Login'))
+                if RegisterValidate(name,lastname,email,password,password2)!="":
+                      
+
+                        return redirect(url_for('userRegister'))
+                else:
+                        cur = mysql.connection.cursor()
+                        cur.execute("SELECT * FROM users WHERE email=%s",(email,))
+                        userinfo = cur.fetchone()
+                        cur.close()
+                        if userinfo is None:
+                                cur = mysql.connection.cursor()
+                                cur.execute('INSERT INTO users (name, lastname, email, password) VALUES (%s,%s,%s,%s)', (name,lastname,email,password))
+                                mysql.connection.commit()
+                                flash('Registrado correctamente')
+                                print("Registrado")
+                                # session['user'] = name
+                                return redirect(url_for('Login'))
+                        else:
+                                flash('El usuario ya existe')
+                                print("Ya existe")
+                                return redirect(url_for('userRegister'))  
         else:
                 return redirect(url_for('index'))
 
